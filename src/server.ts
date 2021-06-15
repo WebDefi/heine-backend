8898b461c0cea78e51617f29c197f69dea1b6ca4
kostiantynn <kostya.code@gmail.com>
2021-06-15 11:49:56 +0300

    added support for static files | volume creation

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import fastifyCors from "fastify-cors";
+import { join, resolve } from "path";
 import accessories from "./routes/accessories";
 import news from "./routes/news";
 import products from "./routes/products";
@@ -10,20 +11,8 @@ server.register(fastifyCors, { origin: true });
 server.register(products, { prefix: "/products" });
 server.register(accessories, { prefix: "/accessories" });
 server.register(news, { prefix: "/news" });
-server.register(require("fastify-swagger"), {
-  prefix: "swagger",
-  swagger: {
-    info: {
-      title: "Swagger for API integratoin",
-      description: "testing the fastify swagger api",
-      version: "0.1.0",
-    },
-    host: "localhost",
-    schemes: ["https"],
-    consumes: ["application/json"],
-    produces: ["application/json"],
-  },
-  exposeRoute: true,
+server.register(require("fastify-static"), {
+  root: join(resolve(__dirname, "../"), "static")
 });
 
 export default server;
