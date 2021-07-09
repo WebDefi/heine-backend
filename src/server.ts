61b5103828afc819f7043c054344d153c4c4ce2e
kostiantynn <kostya.code@gmail.com>
2021-07-09 14:38:10 +0300

    lang switch by cookie integration OnSend hook

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { join, resolve } from "path";
 import accessories from "./routes/accessories";
 import news from "./routes/news";
 import products from "./routes/products";
+import cookie from "fastify-cookie";
 
 const server = fastify({ logger: false });
 
@@ -11,8 +12,11 @@ server.register(fastifyCors, { origin: true });
 server.register(products, { prefix: "/products" });
 server.register(accessories, { prefix: "/accessories" });
 server.register(news, { prefix: "/news" });
+server.register(cookie, {
+  secret: "aloha",
+});
 server.register(require("fastify-static"), {
-  root: join(resolve(__dirname, "../"), "static")
+  root: join(resolve(__dirname, "../"), "static"),
 });
 
 export default server;
