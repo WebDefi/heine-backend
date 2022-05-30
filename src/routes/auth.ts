8e306fd2a3eaea71557a09b144a6dddc69e8e335
kostiantynn <kostya.code@gmail.com>
2022-05-30 23:02:38 +0300

    hard code values

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,7 +4,7 @@ const auth: FastifyPluginCallback = async (fastify: FastifyInstance) => {
   fastify.post("/authenticate", async (req: any, res: FastifyReply) => {
     let user: string = req.body.user;
     let password: string = req.body.password;
-    if (user === process.env.AUTH_USER && password === process.env.AUTH_PASS) {
+    if (user === "karas123" && password === "putinClown994457") {
       let token = Buffer.from(`${user}:${password}`).toString("base64");
       res.status(200).send({ token });
     } else {
@@ -18,7 +18,7 @@ const authHook = async (req: any, res: any, done: any) => {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = Buffer.from(token, "base64").toString("ascii");
     const [user, password] = decodedToken.split(":");
-    if (user === process.env.AUTH_USER && password === process.env.AUTH_PASS) {
+    if (user === "karas123" && password === "putinClown994457") {
       done();
     } else {
       res.status(403).send({ error: "Invalid credentials" });
